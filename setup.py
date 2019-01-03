dbcec70a301ba16d12ec6f6164c971c541f4bdc4
Franklin Sarmiento <franklinitiel@gmail.com>
2019-01-03 15:07:13 -0500

    Stable version defined.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="django2_semantic_ui",
-    version="1.1.2",
+    version="1.2.0",
     author="Franklin Sarmiento",
     author_email="franklinitiel@gmail.com",
     description="Library to easy install, configure and use Semantic UI framework with Django project (Python 3.x)",
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/franklintiel/django2-semantic-ui/wiki",
     license="MIT",
     classifiers=[
-        # "Development Status :: 4 - Beta",
+        #"Development Status :: 4 - Beta",
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.6",
